f669f1bc8106c810479cd2a50587598b7abd212a
Lucas Marques <lucasmarquesds@gmail.com>
2015-04-21 10:16:59 -0300

    connecting to bds

---
+++ settings.py
@@ -0,0 +1,15 @@
+"""
+Sync settings
+"""
+
+# Cassandra Connection
+CASSANDRA_CONNECTION = {
+    'contact_points': ['127.0.0.1'],
+    'port': 9042,
+}
+
+# ElasticSearch Connection
+ELASTICSEARCH_CONNECTION = {
+    'host': '127.0.0.1',
+    'port': 9200,
+}
