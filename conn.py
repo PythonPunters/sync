02deeba5b6779926b51afc1d1b13f30864b92763
Lucas Marques <lucasmarquesds@gmail.com>
2015-04-21 10:18:34 -0300

    connecting to bds

--- conn.py
+++ conn.py
@@ -2,10 +2,12 @@ from settings import *
 from cassandra.cluster import Cluster
 import elasticsearch
 
+
 def get_cassandra_conn():
     cluster = Cluster(**CASSANDRA_CONNECTION)
     session = cluster.connect("sync")
     return session
+
 
 def get_elasticsearch_conn():
     es = elasticsearch.Elasticsearch(**ELASTICSEARCH_CONNECTION)
