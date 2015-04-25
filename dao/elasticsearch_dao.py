79107e8f01111f067a31793d1881d1950746493f
Lucas Marques <lucasmarquesds@gmail.com>
2015-04-25 15:33:06 -0300

    improving elasticsearch dao

--- dao/elasticsearch_dao.py
+++ dao/elasticsearch_dao.py
@@ -1,11 +1,11 @@
+from elasticsearch.exceptions import ConnectionError
 from settings import *
 import elasticsearch
 import logging
-import uuid
 import time
 
 # starting the logger
-logger = logging.getLogger("ElasticSearchDAO")
+logger = logging.getLogger("elasticsearch_dao")
 
 
 class ElasticSearchDAO():
@@ -24,11 +24,14 @@ class ElasticSearchDAO():
         :param name: Name of the doc_typel
         :return the status of document creation
         """
-        if name not in self.__get_doc_types():
-            logger.info("Creating a new doc_type")
-            return self.es.create(DATABASE, doc_type=name, body={}, id=time.time())
-        else:
-            logger.error("doc_type already exists!")
+        try:
+            if name not in self.__get_doc_types():
+                logger.info("Creating a new doc_type")
+                return self.es.create(DATABASE, doc_type=name, body={}, id=time.time())
+            else:
+                logger.error("doc_type already exists!")
+        except Exception as ex:
+            logger.error("An error ocurred. More info: %s", ex)
 
     def __get_all_data(self, doc_type=None):
         """
@@ -37,9 +40,12 @@ class ElasticSearchDAO():
         :return a bit cleaned list of existing documents
         """
         result = []
-        for r in self.es.search(index=DATABASE, doc_type=doc_type)['hits']['hits']:
-            logger.info("Found document %s", r)
-            result.append(r)
+        try:
+            for r in self.es.search(index=DATABASE, doc_type=doc_type)['hits']['hits']:
+                logger.info("Found document %s", r)
+                result.append(r)
+        except Exception as ex:
+            logger.error("An error ocurred. More info: %s", ex)
         return result
 
     def __cleaned_data(self, doc_type=None):
@@ -49,9 +55,12 @@ class ElasticSearchDAO():
         :return a list of documents without any extra information
         """
         result = []
-        for r in self.__get_all_data(doc_type=doc_type):
-            logger.info("Found document _source %s", r)
-            result.append(r['_source'])
+        try:
+            for r in self.__get_all_data(doc_type=doc_type):
+                logger.info("Found document _source %s", r)
+                result.append(r['_source'])
+        except Exception as ex:
+            logger.error("An error ocurred. More info: %s", ex)
         return result
 
     def __get_doc_types(self, doc_type=None):
@@ -61,9 +70,12 @@ class ElasticSearchDAO():
         :return list of all doc_types
         """
         doc_types = []
-        for r in self.__get_all_data(doc_type=doc_type):
-            logger.info("Found doc_type %s", r)
-            doc_types.append(r)
+        try:
+            for r in self.__get_all_data(doc_type=doc_type):
+                logger.info("Found doc_type %s", r)
+                doc_types.append(r)
+        except Exception as ex:
+            logger.error("An error ocurred. More info: %s", ex)
 
         return doc_types
 
@@ -75,11 +87,15 @@ class ElasticSearchDAO():
         :param id: Document id. If it's none, insert data to an document, else update it
         :return the status of document insert/update
         """
-        if not id:
-            # create a new document
-            logger.info("Creating a new document")
-            return self.es.index(index=DATABASE, doc_type=doc_type, body=body, id=time.time())
-        else:
-            # update the document
-            logger.info("Updating the document with id: %f", id)
-            return self.es.index(index=DATABASE, doc_type=doc_type, body=body, id=id)
+        try:
+            if not id:
+                # create a new document
+                if body not in self.__cleaned_data():
+                    logger.info("Creating a new document")
+                    return self.es.index(index=DATABASE, doc_type=doc_type, body=body, id=time.time())
+            else:
+                # update the document
+                logger.info("Updating the document with id: %f", id)
+                return self.es.index(index=DATABASE, doc_type=doc_type, body=body, id=id)
+        except Exception as ex:
+            logger.error("An error ocurred. More info: %s", ex)
